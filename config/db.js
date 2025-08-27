9c3567cf7bc2e2743d293dc9bd4a347e64fc9af0
petersonmaina-dev <mainapeterson62@gmail.com>
2025-08-27 19:31:17 +0300

    Fix blogdetails view rendering

--- config/db.js
+++ config/db.js
@@ -1,15 +1,11 @@
-// db.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error("❌ MongoDB Connection Error:", err.message);
+  } catch (error) {
+    console.error(`❌ Error: ${error.message}`);
     process.exit(1);
   }
 };
